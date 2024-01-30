1f663cfc99066477e51e9c5279563ade252aac01
smartinez2324 <smartinez@ifp-alguero.org>
2024-01-30 20:38:24 +0100

    hoy

---
+++ wordpress2/wp-content/themes/portafoli/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfoli Jordi</title>
+	
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>/css/style.css" />
+	
+	
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript">
+        function sound(surl) {
+            document.getElementById("myspan").innerHTML =
+            "<embed src="+surl+" hidden=true autostart=true loop=false style="">";
+        }
+    </script>
+</head>
+<body>
+    <ul>
+        <a id="myspan" href="mp08_UF1_01_inici_moraj/index.html" onMouseOver="sound('syria.mp3')" />
+            <li class="practica">Inici</li>
+        </a>
+        <a href="mp08_UF1_02_horari_moraj/index.html">
+            <li class="practica">Horari</li>
+        </a>
+        <a href="mp08_UF1_03_feedly_moraj/index.html">
+            <li class="practica">Feedly</li>
+        </a>
+        <a href="mp08_UF1_04_ifpagram_moraj/index.html">
+            <li class="practica">ifpagram</li>
+        </a>
+        <a href="mp08_UF1_05_escacs_moraj/index.html">
+            <li class="practica">Escacs</li>
+        </a>
+        <a href="mp08_UF1_06_absolute_moraj/index.html">
+            <li class="practica">Absolute</li>
+        </a>
+        <a href="mp08_UF1_07_horari_moraj/index.html">
+            <li class="practica">Floreta</li>
+        </a>
+        <a href="mp08_UF1_08_floreta_moraj/index.html">
+            <li class="practica">Cara</li>
+        </a>
+        <a href="mp08_UF1_09_cara_moraj/index.html">
+            <li class="practica"></li>
+        </a>
+        <a href="mp08_UF1_02_horari_moraj/index.html">
+            <li class="practica"></li>
+        </a>
+    </ul>
+</body>
+</html>
---
+++ wordpress4/wp-content/themes/portfolio/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+
+    <meta name="theme-color" content="#FF9933">
+    <meta name="msapplication-navbutton-color" content="#FF9933">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#FF9933">
+    <link rel="icon" sizes="192x192" href="img/icon.png">
+    <link rel="icon" sizes="512x512" href="img/icon.png">
+    <link rel="apple-touch-icon" href="img/icon.png">
+
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url'); ?>/css/style.css" />
+	
+    <title>Portfoli Stephannie</title>
+</head>
+<body>
+    <img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="">
+    
+	<nav class="menu">
+        <ul class="navegacio">
+            <li class="primernivell"> Casa</li>
+
+            <li class="primernivell"><a href="html/sobremi.html">Sobre mi</a></li>
+
+            <li class="primernivell"> Portafolio
+                <ul class="secundari">
+                    <li class="segonnivell"><a href="html/index.html">MP08</a></li>
+                </ul>
+            </li>
+
+            <li class="primernivell"><a href="html/contacte.html">Contacte</a></li>
+
+        </ul>
+
+         <hr>
+         <br>
+
+         
+    </nav>
+   
+    <h1>PORTFOLI</h1>
+    <p id="linea"></p>
+    <!--<ul>
+        <a href="mp08_UF1_01_martinezs/index.html">
+            <li class="practica">Inici</li>
+        </a>
+        <a href="mp08_UF1_02_horari_martinezs/index.html">
+            <li class="practica">Horari</li>
+        </a>
+        <a href="mp08_UF1_03_feedly_martinezs/index.html">
+            <li class="practica">Feedly</li>
+        </a>
+        <a href="mp08_UF1_04_ifapgram_martinezs/index.html">
+            <li class="practica">Ifapgram</li>
+        </a>
+        <a href="mp08_UF1_05_escacs_martinezs/index.html">
+            <li class="practica">Escacs</li>
+        </a>
+        <a href="mp08_UF1_06_absolute_martinezs/index.html">
+            <li class="practica">Absolute</li>
+        </a>
+        <a href="mp08_UF1_07_floreta_martinezs/index.html">
+            <li class="practica">Floreta</li>
+        </a>
+        <a href="mp08_UF1_08_cara_martinezs/index.html">
+            <li class="practica">Cara</li>
+        </a>
+        <a href="mp08_UF1_09_photoshop/index.html">
+            <li class="practica">Photoshop</li>
+        </a>
+        <a href="mp08_UF1_10_pelys_martinezs/index.html">
+            <li class="practica">Pelys</li>
+        </a>
+    </ul> -->
+    
+</body>
+</html>
